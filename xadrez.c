db36c781710bd08bac1441d2b61d5efea2dfd81e
jsant437 <jdlopes00@gmail.com>
2025-03-20 19:58:50 +0000

    Nível aventureiro - implementação

--- xadrez.c
+++ xadrez.c
@@ -10,6 +10,8 @@ int main() {
     const int casasTorre = 5;
     const int casasBispo = 5;
     const int casasRainha = 8;
+    const int casasCavaloH = 1;
+    const int casasCavaloV = 2;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -43,7 +45,18 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
-
+    casa = 0;
+    printf("\nMovimento cavalo:\n");
+    for(int x = 0; x < casasCavaloH; x++)
+    {
+        while(casa < casasCavaloV)
+        {
+            casa++;
+            printf("Baixo, ");
+        }
+        printf("Esquerda\n");
+    }
+    
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
